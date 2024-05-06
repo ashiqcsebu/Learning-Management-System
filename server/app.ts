a365eb756195cfeb1e5cb45ada154c7bb0840fea
Ashiqul Islam Shabu <ashiq.cse3.bu@gmail.com>
2024-05-07 01:58:00 +0600

    Course Model Done

--- server/app.ts
+++ server/app.ts
@@ -5,16 +5,14 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import { ErrorMiddleware } from "./middleware/error";
 import userRouter from "./routes/user.route";
-
+import courseRouter from "./routes/course.route";
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(ErrorMiddleware);
 app.use(cors({ origin: process.env.ORIGIN }));
 
-
-app.use("/api/v1",userRouter);
-
-
+app.use("/api/v1", userRouter);
+app.use("/api/v1", courseRouter);
 
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
   res.status(200).json({
