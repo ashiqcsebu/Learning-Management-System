dbf4e319487badbf56db539b122ed2c941c7e68e
Ashiqul Islam Shabu <ashiq.cse3.bu@gmail.com>
2024-05-07 12:38:25 +0600

    Course Edit Functionality Done

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -3,6 +3,7 @@ import { CatchAsyncError } from "../middleware/catchAsyncErrors";
 import ErrorHandler from "../utilis/ErrorHandler";
 import cloudinary from "cloudinary";
 import { createCourse } from "../services/course.service";
+import courseModel from "../models/course.model";
 
 export const uploadCourse = CatchAsyncError(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -25,3 +26,90 @@ export const uploadCourse = CatchAsyncError(
     }
   }
 );
+
+export const editCourse = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data = req.body;
+      const courseId = req.params.id;
+      const thumbnail = data.thumbnail;
+
+      if (thumbnail) {
+        if (thumbnail.public_id) {
+          await cloudinary.v2.uploader.destroy(thumbnail.public_id);
+        }
+        // Upload the new thumbnail
+        const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
+          folder: "courses",
+        });
+
+        data.thumbnail = {
+          public_id: myCloud.public_id,
+          url: myCloud.secure_url,
+        };
+      }
+
+      // Use findByIdAndUpdate to update the course
+      const course = await courseModel.findByIdAndUpdate(
+        courseId,
+        { $set: data },
+        { new: true }
+      );
+
+      if (!course) {
+        return next(new ErrorHandler("Course not found", 404));
+      }
+
+      res.status(200).json({
+        success: true,
+        course,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
+
+// export const getSingleCourse = CatchAsyncError(async(req:Request, res:Response, next:NextFunction)=>{
+//     try {
+        
+//     } catch (error: any) {
+//         return next(new ErrorHandler(error.message, 500));
+//       }
+// })
+
+
+// export const editCourse = CatchAsyncError(
+//   async (req: Request, res: Response, next: NextFunction) => {
+//     try {
+//       const data = req.body;
+//       const courseId = req.params.id;
+//       const thumbnail = data.thumbnail;
+//       if (thumbnail) {
+//         await cloudinary.v2.uploader.destroy(thumbnail.public_id);
+//         const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
+//           folder: "courses",
+//         });
+
+//         data.thumbnail = {
+//           public_id: myCloud.public_id,
+//           url: myCloud.secure_url,
+//         };
+//       }
+
+//       const course = await courseModel.findByIdAndUpdate(
+//         courseId,
+//         {
+//           $set: data,
+//         },
+//         { new: true }
+//       );
+//       res.status(200).json({
+//         success: true,
+//         course,
+//       });
+//     } catch (error: any) {
+//       return next(new ErrorHandler(error.message, 500));
+//     }
+//   }
+// );
--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { uploadCourse } from "../controllers/course.controller";
+import { editCourse, uploadCourse } from "../controllers/course.controller";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 const courseRouter = express.Router();
 
@@ -8,6 +8,12 @@ courseRouter.post(
   isAuthenticated,
   authorizeRoles("admin"),
   uploadCourse
+);
+courseRouter.put(
+  "/edit-course/:id",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  editCourse
 );
 
 export default courseRouter;
